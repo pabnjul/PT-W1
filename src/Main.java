1c945148bf4bc0f2551ee4a792c9795331e177e0
Julie Palmandez <inocenteypura@gmail.com>
2022-10-28 16:49:53 -0700

    first and a half commit being made

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//create double variables and assign values
+		double value1 = 14.99;
+		double value2 = 15.99;
+		
+		//add the two values together and print to the console
+		System.out.println(value1 + value2);
+		
+		
 
 	}
 
